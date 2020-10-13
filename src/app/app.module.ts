b20a6fbf5b5d548e1fd7b12fa2a81609c2f78a8a
pasha13730 <ah532703@gmail.com>
2020-10-13 18:03:18 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { FormsModule } from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+
 import { MatSliderModule } from '@angular/material/slider';
 import {MatBadgeModule} from '@angular/material/badge';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ,BrowserAnimationsModule,MatSliderModule,MatBadgeModule],
+  imports:      [ BrowserModule, FormsModule ,BrowserAnimationsModule,MatSliderModule,MatBadgeModule,MatButtonModule,MatDividerModule,MatIconModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
+  
 })
 export class AppModule { }
